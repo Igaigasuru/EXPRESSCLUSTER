0800a161e7b7c79eedb607f41e1aadec6b9033f3
Rei Iga <31835726+Igaigasuru@users.noreply.github.com>
2019-02-05 14:02:41 +0900

    Create hostexec2_genw.sh

---
+++ sap/NW/Lin/script/genw/hostexec2_genw.sh
@@ -0,0 +1,22 @@
+#****************************************************************
+#*      hostexec     genw.sh             (Version : 4.1-1)      *
+#****************************************************************
+
+ulimit -s unlimited
+
+SAPHOSTEXEC="/usr/sap/hostctrl/exe/saphostexec"
+
+TARGETS="saphostexec sapstartsrv saposcol"
+
+for TARGET in ${TARGETS}
+do
+	RESULT=`${SAPHOSTEXEC} -status 2>&1 | grep "${TARGET}"`
+	STATUS=`echo ${RESULT} | cut -f2 -d" "`
+	PID=`echo ${RESULT} | cut -f5 -d" "`
+	if [ "${STATUS}" != "running" -o "${PID}" = "0)" ]
+	then
+		exit 1
+	fi
+done
+
+exit 0
