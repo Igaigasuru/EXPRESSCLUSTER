49c25219e37d743838cb09b9a34bb7e5d08543e9
Rei Iga <31835726+Igaigasuru@users.noreply.github.com>
2019-02-05 13:48:23 +0900

    Create ASCS_instance_ENQ_genw.sh

---
+++ sap/NW/Lin/script/genw/ASCS_instance_ENQ_genw.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+#*******************************************************
+#*      ASCS ENQ           genw.sh   (Version : 4.1-1) *
+#*******************************************************
+
+ulimit -s unlimited
+
+#***************************************
+# INSTANCE      : ASCS instance name
+# CHECK_MONNAME : Monitor resource name for checking shared filesystem
+# DIR_PATH      : Directory path of check_monitor_status.sh
+#***************************************
+
+#***************************************
+INSTANCE="NEC_ASCS10_ascssv"
+CHECK_MONNAME="diskw-NFS"
+DIR_PATH="/opt/nec/clusterpro/scripts/utility"
+#***************************************
+
+TARGET="enserver"
+
+#***************************************
+
+CONFFILE="/opt/nec/clusterpro/etc/clp_shi_connector.conf"
+
+SID=`echo "${INSTANCE}" | cut -d_ -f1`
+INAME=`echo "${INSTANCE}" | cut -d_ -f2`
+INO=`echo "${INAME}" | sed 's/.*\([0-9][0-9]\)$/\1/'`
+SAPUSER=`echo "${SID}adm" | tr "[:upper:]" "[:lower:]"`
+
+# Check monitor status for shared filesystem
+if [ -n "${CHECK_MONNAME}" -a "${CHECK_MONNAME}" != "<DISKW>" ]; then
+	if [ ! -e ${DIR_PATH}/check_monitor_status.sh ]; then
+		/usr/sbin/clplogcmd -m "${DIR_PATH}/check_monitor_status.sh does not exist." -l warn
+		exit 0
+	fi
+	${DIR_PATH}/check_monitor_status.sh "${CHECK_MONNAME}"
+	if [ $? -ne 0 ]; then
+		/usr/sbin/clplogcmd -m "Skip monitoring ${INSTANCE} instance(enq) due to the monitor resource ${CHECK_MONNAME} is not normal." -l warn
+		exit 0
+	fi
+fi
+
+YELLOW_AS_ERROR=1
+if [ -f "${CONFFILE}" ]
+then
+	. ${CONFFILE}
+	if [ "${YELLOW_AS_ERROR}" = "" ]
+	then
+		YELLOW_AS_ERROR=1
+	fi
+	if [ "${ENSA_VERSION}" != "" -a "${ENSA_VERSION}" != "1" ]
+	then
+		TARGET="enq_server"
+	fi
+fi
+
+# Check instance status
+LINE=`su - ${SAPUSER} -c "sapcontrol -nr ${INO} -function GetProcessList" | grep "${TARGET}"`
+COLOR=`echo ${LINE} | cut -f3 -d, | sed 's/^[ \t]*//g'`
+if [ "${COLOR}" != "GREEN" ]
+then
+	if [ "${COLOR}" != "YELLOW" ]
+	then
+		exit 1
+	else
+		if [ ${YELLOW_AS_ERROR} -ne 0 ]
+		then
+			exit 1
+		fi
+	fi
+fi
+
+exit 0
